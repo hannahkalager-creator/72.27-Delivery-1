28ebd351098d1edf5c699cee014af06a4038234c
Stellafk <stellafk@stud.ntnu.no>
2026-08-21 18:46:21 -0300

    main fix

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ neighbors = world.get_neighbors(world.start)
 print("Possible moves from start:", neighbors)
 
 #run BFS
-
 path, expanded_nodes, frontier_nodes, processing_time = bfs(world)
 
 visualize(world, path)
