e502c743405e34bbae4006b12f9d3650a34bccd0
Albertine <albertine.gjos@orbitntnu.com>
2026-08-23 22:37:43 -0300

    Added space

--- heuristics.py
+++ heuristics.py
@@ -11,6 +11,7 @@ def euclidean_distance(state, goal):
     The Euclidean distance is the straight-line distance between two points in a 2D space"""
     return math.sqrt((state[0] - goal[0])**2 + (state[1] - goal[1])**2)
 
+
 def weighted_manhattan(state, goal):
     """This function calculates a weighted Manhattan distance between the current state and the goal state.
     The weighted Manhattan distance is defined as twice the sum of the absolute differences of the Cartesian coordinates."""
