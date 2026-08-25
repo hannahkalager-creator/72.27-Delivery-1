8c7deb8322dd9d5c5b830e0af34b70097213df9a
Albertine <albertine.gjos@orbitntnu.com>
2026-08-25 11:35:47 -0300

    Removed Print statements from main.py

--- main.py
+++ main.py
@@ -16,30 +16,8 @@ from game_visual import visualize
 def report_multi_agent(search_method, result):
     """This function generates a report for the multi-agent pathfinding results.
     It takes the search method name and the result of the search algorithm as input."""
-    path, expanded_nodes, frontier_nodes, processing_time = result
-
-    print(f"\n{search_method}")
-    print("Solution:", path)
-    print("Expanded nodes:", expanded_nodes)
-    print("Frontier nodes:", frontier_nodes)
-    print("Processing time:", processing_time, "seconds")
-
-    if not path:
-        print("No solution found.")
-        return
-
-    print("Turn cost:", len(path) - 1)
-
-    agent_costs = calculate_agent_costs(path)
-
-    print("Movement cost per agent:")
-    for agent_index, cost in enumerate(agent_costs):
-        print(f"Agent {agent_index}: {cost}")
-
-    print("Total movement cost:", sum(agent_costs))
-    
+    path, _, _, _ = result
     agent_paths = [[state[0][agent_index] for state in path] for agent_index in range(len(path[0][0]))]
-    
     visualize(search_method, multi_world, agent_paths)
 
 
